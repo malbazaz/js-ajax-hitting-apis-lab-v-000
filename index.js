0213658ce52524d3f03830055e8a7b1b6cb6c794
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 13:55:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function getRepositories(){
   const req = new XMLHttpRequest
+  username = document.getElementById("username")
   req.open("GET", `https://api.github.com/${username}/repos`)
   req.send()
+    req.addEventListener("load", showRepositories)
 }
