194b49b74078a7ac8d9274e0ba279dae889b31b0
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 16:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function displayRepositories(event, data){
 }
 
 function getCommits(el) {
-  const name = el.dataset.fullname
+  const name = el.dataset.full_name
   console.log("name", name)
   const req = new XMLHttpRequest()
   let username = document.getElementById("username").value
