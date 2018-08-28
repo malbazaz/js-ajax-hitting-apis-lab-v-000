126319fadb9ee83a922a0bd1e20ea1cc1edb4c1f
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 14:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function getRepositories(){
 function displayRepositories(event, data){
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  let username = document.getElementById("username")
+  const repoList = `<ul>${repos.map(r => '<li>' +`<a href = https://github.com/${username}` + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
