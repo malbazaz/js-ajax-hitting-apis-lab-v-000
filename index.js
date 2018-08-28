b60d2bd582a96a096c0571944ba7ae4a68c82326
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 16:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ function getCommits(el) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+console.log(commitsList)
   document.getElementById("commits").innerHTML = commitsList
 }
+
 function getBranches(el) {
   debugger;
   const name = el.dataset.repo
