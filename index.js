e4653039a77150edaffd817e4a74094573925ad3
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 17:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,8 @@ function displayRepositories(){
      data-username="${r.owner.login}"
      onclick='getBranches(this)'> Get Branches </a>
      </li>`
-  }).join(' ')
-  const repoList = `<ul> ${repos.map(r => '<li>' + `<a href = https://github.com/users/${username}/${r.name}>`+ r.name +'</a>'+' ' + '<a href="#"' + 'onclick="getCommits(this)"> Get Commits </a> </li>').join('')}</ul>`
-  const branchesList =  `<ul>${repos.map(r => '<li>' + `<a href= https://github.com/repos/${username}/${r.name}/branches`+ username + '</a>' +' '+ r.name +'<a href="#"'+'onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList + branchesList
+  }).join('')
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
