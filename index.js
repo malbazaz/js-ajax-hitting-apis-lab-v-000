e2426e75f3b398d80030ef283fcaf3f529120f4d
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 14:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,5 +41,5 @@ function getBranches(el) {
 function displayBranches() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitsList
+  document.getElementById("details").innerHTML = commitsList
 }
