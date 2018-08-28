97af47d351b5f29ea72b349a0325a1bcc4430064
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 16:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function getCommits(el) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const list = commits.map(commit => {
-    return `<li> user:${commit.author.login}, name:${commit.commit.name}
+    return `<li> user:${commit.author.login}, name:${commit.commit.authorname}
   })
 
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.message + '</li>').join('')}</ul>`
