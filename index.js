5dbbbab08125f48a053750c021d462dbf62dcac5
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-28 14:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function getCommits(el) {
   req.open("GET", 'https://api.github.com/repos/'+ username +'/' + name + '/commits')
   req.send()
 }
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
